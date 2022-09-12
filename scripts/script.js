cc544ebbfca8fb856a336ffbaeb5faa5080548aa
ougheoke <111533969+ougheoke@users.noreply.github.com>
2022-09-12 15:14:04 -0500

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -4,15 +4,29 @@ $(document).ready(function () {
   $("#dogeOne").click(function () {
     $("#outputDiv").hide();
 
-    console.log("user clicked dogeOne 2");
-    $("#outputMsg").text("You Lose, you Stupid Loser");
-    $("#outputDiv").toggle();
+    console.log("user clicked doge1");
+    $("#outputMsg").text("Not enough WOW, Try again.");
+    $("#outputDiv").toggle().fadeIn("fast");
   });
 
   $("#dogeTwo").click(function () {
     $("#outputDiv").hide();
-    console.log("user clicked dogeTwo 2");
-    $("#outputMsg").text("You has WOW. So much WOW!! ");
-    $("#outputDiv").toggle();
+    console.log("user clicked doge2");
+    $("#outputMsg").text("In the name of the WOW, WOW, and WOW; you has so much WOW");
+    $("#outputDiv").toggle().fadeIn("fast");
+  });
+
+  $("#dogeThree").click(function () {
+    $("#outputDiv").hide();
+    console.log("user clicked doge3");
+    $("#outputMsg").text("You call that WOW?? Smh.");
+    $("#outputDiv").toggle().fadeIn("fast");
+  });
+
+  $("#dogeFour").click(function () {
+    $("#outputDiv").hide();
+    console.log("user clicked doge4");
+    $("#outputMsg").text(" Nah. But he looks cool though, right?");
+    $("#outputDiv").toggle().fadeIn("fast");
   });
 });
